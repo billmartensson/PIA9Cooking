02bcfdd570d7f0c324b9a65b52ecbf8603ebfe8b
Bill Mårtensson <bill@magictechnology.se>
2021-03-25 09:13:28 +0100

    Unit test. Description

--- app/src/main/java/se/magictechnology/pia9cooking/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia9cooking/MainActivity.kt
@@ -3,6 +3,8 @@ package se.magictechnology.pia9cooking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -17,17 +19,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     // Här skriver jag lite viktig kod
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        firebaseAnalytics = Firebase.analytics
+
+
         auth = Firebase.auth
 
         supportFragmentManager.beginTransaction().add(R.id.mainFragmentLayout, StartFragment()).commit()
 
         findViewById<TextView>(R.id.tabRecipeTV).setOnClickListener {
+
             supportFragmentManager.beginTransaction().replace(R.id.mainFragmentLayout, StartFragment()).commit()
         }
 
